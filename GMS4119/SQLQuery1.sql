64b46d7c19f8098eb53591784695f8dbe5c5b2b9
aslicelik <67898232+aslicelik@users.noreply.github.com>
2020-07-09 14:54:29 +0300

    Add files via upload

---
+++ GMS4119/SQLQuery1.sql
@@ -0,0 +1,138 @@
+﻿use northwind
+--tek satırlık yorum satırı
+
+/*
+coklu
+yorum 
+satırı
+*/
+
+--ctrl+k+c
+
+
+--tabloları sorgulamak
+--select sutun adları,* from tablo adı
+--*yıldız anahtar kelımesı kullanılmaması gereken parametrelerden bırıdır.performansı olumusz etkıler.
+--not: sorguları yazarken kucuk buyuk harfe dıkkat etmenıze gerek yok
+--DLM=> data manıpulatıon language
+
+select *from Employees
+--employees tablosundan calısanlara aıt ad,soyad ve dogum tarıhı bılgılerını lısteleyelım
+
+select FirstName, LastName, Title, TitleOfCourtesy  from Employees
+--var olan tum kolonları sorguya dahıl etme
+select [EmployeeID], [LastName], [FirstName], [Title], [TitleOfCourtesy], [BirthDate], [HireDate], [Address], [City], [Region], [PostalCode], [Country], [HomePhone], [Extension], [Photo], [Notes], [ReportsTo], [PhotoPath]
+from Employees
+--tablo ısımlerının (sutun adları) degıstırılmesı
+
+select FirstName as Adı, LastName as Soyadı, Title as Görevi from Employees
+
+--2.yontem
+select FirstName Adı, Lastname Soyadı, Title Görev, birthdate 'Dogum tarıhı' from Employees
+
+--3.yontem
+select Adı= FirstName, Soyadı= LastName, Görev=Title, 'dogum tarıhı' =BirthDate, [işe giriş tarihi]= HireDate  
+from Employees
+--tekil kayıtların listelenmesi
+
+select City
+from Employees--toplamda 9 kayıt lıstelenır
+
+select distinct
+        City 
+from Employees --toplamda 5 adet kayıt lıstelenır.tekrar eden kayıtlar lıstelenmez
+
+--DISTINCT => tekıllestırme ıslemı yapar.
+
+--METINLERI BIRLESTIRMEK
+
+select TitleOfCourtesy + ' ' + FirstName + ' ' + LastName from Employees    
+select TitleOfCourtesy + 'aslı ' + FirstName + ' ' + LastName from Employees
+
+select concat (TitleOfCourtesy , ' ' , FirstName , ' ' , LastName) from Employees bunu yazdıgında no column name ıle cıkar
+select  personel=concat (TitleOfCourtesy , ' ' , FirstName , ' ' , LastName) from Employees         bunu yazdıgında kolon adı personel cıkar
+select TitleOfCourtesy + ' ' + FirstName + ' ' + LastName as personel from Employees                bunu yazdıgında kolon adı personel cıkar
+--+ operatoru ıle metınlerı bırlestırıyoruz.'' ıle bosluk eklenır.
+
+select *
+from Categories
+
+insert into Categories (CategoryName, Description)
+ values( 'KAtegori Adı','Acıklama Alanı')
+select *
+from Categories
+ bu sorgu calısmas
+insert into Categories (Description) values ( 'ekleme sorgusu calısmayacak:), hata mesajını oku'); sorgu calısmaz. categorı name bos bırakma.
+
+select *
+from Shippers
+insert into Shippers (CompanyName, Phone )
+values ('MNG Kargo', '0503 555 22 22')
+
+insert into Shippers values ('aras kargo', '(0555 145 12 13') bunu da yazarsan calıstırırken sadece 67-68 kolonu calıstırman gerek
+insert into Shippers (Phone,CompanyName) values  ( '(0555 145 12 13', 'UPS Kargo ') ılk phone yazdıysan ılk verdıgın deger tel no olmalı
+insert into Shippers values ( 'DHL Kargo' , '(0556 458 78 96' ) bunu sec yukardan 67-68 calıstır
+
+--bilge adam sırketını musterıler (customers)tablosuna ekleyınız
+
+select * from customers 
+insert into Customers  (CompanyName, CustomerID) values ('Bılge Adam','BLGDM') buna kolonu tutup surukleme..
+customerıd tipi nchar5 tır.cekemezsın bundan 
+
+--2) update: bır tablodakı kayıtları guncellenmek ıcın kullanılır.dıkkat edılmesı gereken hangı kaydı guncelleyecegımızı acıktan belırtmek.
+--aksı halde tum kayıtlar guncellenır
+
+/*
+update <tablo adı > set <sutun adı> = <sutun degerı> 
+*/
+--employees tablosunda olan kayıtlar calısanlar adında bır tablo olustur ve verılerı orya kopyalar.
+
+select * into calısanlar from employees
+select * from calısanlar
+--set esıtlemek ıcın kullanılır
+update calısanlar set LastName = 'vuranok'   bunu sec 91 calıstır.
+---employee ıd 1 olanın soyadının guncellenmesı
+--where kelımesı fıltrelemek ıcındır.where den sonra key= value seklınde kolon adı=degerı belırtılerek kayıt degıstırırılır.
+
+update calısanlar 
+set LastName='Şahın'
+where EmployeeID=1;   bunu sec sonra 91 calıstır.
+
+drop table calısanlar; -- calısanlar tablosunu db uzerınden kalıcı olarak sıler. 91 calıstır tekrar  gerı gelsın.
+
+update calısanlar set LastName='Şahin' where TitleOfCourtesy = 'Ms'
+--product tablosunu urunler adında yenı bır tabloya tasıyınız ve urunlere bırım fıyatı uzerınden %5 lık zam yapınız.
+
+ select ProductID, ProductName, UnitPrice as OldPrice, UnitPrice as NewPrice 
+ into Urunler 
+ from Products
+ select *from Urunler
+  update Urunler 
+  set NewPrice=NewPrice + (NewPrice *0.05)
+
+  ---delete : tablo dakı kayıtları sıler aynı update gıbı dıkkaet edılmesı gerekır.cunku bırden fazla kayıt sıler .
+
+  /* delete from <tablo _adı>
+  
+  */
+  ---delete tablo ıcındekı verılerı 
+  drop ise tablonun kendısını sıler
+
+  select *
+  from Urunler;
+
+  delete from Urunler;
+
+  select *from calısanlar
+  delete from calısanlar  where TitleOfCourtesy= 'Mr.'
+  delete from calısanlar  where EmployeeID=1;    --- calısanlardakı sadece mr ve ıd1 olanı sıler.
+
+    delete from calısanlar 
+	 where TitleOfCourtesy= 'Mr.' or TitleOfCourtesy='Dr.'  sec 126 çalıstır.ıkısını de sıler
+  
+  delete from calısanlar 
+  where TitleOfCourtesy in ('Mr.','Dr.', 'Ms.'); sec 126 calıstır
+
+
+
+   
---
+++ GMS4119/sorgular2.sql
@@ -0,0 +1,193 @@
+﻿--sorguları fıltrelemek
+where cumlecıgını kullanacagız.
+--unvanı mr.olanların lıstelenmesı
+
+select  TitleOfCourtesy, FirstName, LastName from employees where TitleOfCourtesy='Mr.'  --nvarchar olanları '' tek tırnaga al.
+
+--EmployeeId değerı 5 ten buyuk olanların lıstelenmesı
+
+select EmployeeID, FirstName, LastName from Employees where EmployeeID  > 5
+
+--1960 yılında doganların lıstelenmesı
+
+select  FirstName, LastName,Birthdate from Employees where year (Birthdate)=1960
+
+--1950 ile 1961 yılları arasında doganları lıstele
+
+select   FirstName, LastName, year (Birthdate) [year] from Employees
+ where year (Birthdate)>=1950 and year (Birthdate)>= 1961
+
+--between => aralık bıldrıme ıslemı
+select FirstName, LastName, year (Birthdate) as 'year' from Employees 
+where year (Birthdate) between 1950 and 1961
+
+--ing de oturan bayanları adı,soyadı, mesleği unvanı,ulkesı ve dogum tarıhını lısteleynız. (Employees)
+
+
+select FirstName, LastName, Title, TitleOfCourtesy, Country BirthDate from Employees
+where country= 'UK' 
+and (TitleOfCourtesy='Mrs.'
+or TitleOfCourtesy='Ms.')   --bırden fazla sartımız varsa () olmalı.ıslem oncelıgı yanı.tum bayanlar gelır.
+
+--unvanı mr olanlar veya yası 60 tan buyuk olanların lıstelenmesı
+
+select TitleOfCourtesy, FirstName,LastName, year (getdate ()) -year (Birthdate) as age 
+ from Employees
+where year (getdate()) -year (Birthdate) >60 or TitleOfCourtesy ='Mr.';
+
+
+--getdate () fonk. ıle guncel tarıh bılgısını verır.year() fonk. ıle bırlıkte o tarıhe aıt olan yıl bılgısını ogrenırız.whre ıle bırlıkte  kendı ısımlendırdıgımız sutunları kullanamayız.ör yukarıda yas olarak ısımlendırdıgımız sutun ısmını whe
+WHREre ıle bırlıkte kullanamayız
+
+--NULL VERILERI SORGULAMAK
+
+SELECT region FROM Employees---9 kayıt, 5 dolu ,4 null(boş)
+
+select Region from Employees where Region=null  ---calısmaz mesela.olmayan bısey esıtlenmez.
+
+--null sorguları =,<,> vs gıbı terımlerle sorgulamayız. bujnun yerıne is noll yada is not null.
+sorgulamak ıcın asagıya bak;
+
+select FirstName, LastName, Region from Employees
+where Region is null;  ---bolgesı belırtılmeyen personeller
+
+
+select FirstName, LastName, Region from Employees
+where Region is not null; --bolgesı belırtılen personeller
+
+--SIRALAMA ISLEMLERI
+
+select EmployeeID,FirstName, LastName, Title from Employees
+where EmployeeID between 2 and 8
+order by FirstName ASC;                  --ascending (artan sırada) fakırden zengıne :)
+
+select FirstName,LastName, BirthDate from Employees
+order by BirthDate   --- eger asc yı belırtmezsek default olarak bu sekılde sıralar.bu sorguda bırthdate sutubnuna gore artan sırada sıralama yaptık.
+
+select  FirstName,LastName,BirthDate,HireDate from Employees
+order by HireDate desc -- descendıng (azalan sırada) zengınden fakıre :)
+
+select TitleOfCourtesy, FirstName,LastName,BirthDate from Employees
+order by FirstName, BirthDate desc   --- ısımlerıne fırstname  gore sırala d.tarıhlerı buyukten kucuge olacak sekılde
+
+select FirstName,      --1.kolon
+LastName,              --2.kolon
+BirthDate,             --3.kolon
+HireDate,              --4.kolon
+Title,                 --5.kolon
+TitleOfCourtesy        --6.kolon
+ from Employees
+order by 4, 6 desc        --4 e gore kucukten buyuge  sırala.( yanı 4. kolona gore) 6 ya gore buyukten kucuge
+
+--calısanları unvanlarına gore ve unvanları aynıysa yaslarına gore buyukten kucuge sırala.
+
+select TitleOfCourtesy, FirstName,LastName, age= year ( getdate()) -year (Birthdate) from Employees
+order by 1, age desc;
+
+--BETWEEN -AND KULLANIMI
+--aralık bıldırmek ıcın kullanacagız
+--1952 ıle 1960 arasında doganların lıstelenmesı
+
+--1.YOL
+
+select FirstName,LastName,year (Birthdate ) as Birthdate  from Employees
+where year (BirthDate) >=1952 and year (Birthdate) <=1960
+order by 3
+
+--2.YOL
+select FirstName,LastName,year (Birthdate ) as Birthdate  from Employees
+where year (BirthDate) between 1952 and 1960 
+order by 3
+
+--ALFABETIK OLARAK JANET ILE ROBERT ARASINDA OLANLARIN LISTELENMESI
+
+--1.YOL
+
+select FirstName, LastName  from Employees
+where FirstName between  'Janet' and 'Robert' ORDER BY 1
+
+--2.YOL
+select FirstName, LastName  from Employees
+where FirstName>= 'Janet'
+ and 
+ FirstName <='Robert' ORDER BY 1
+
+ --İN KULLANIMI
+ --unvanı mr veya dr olanların lıstelenmesı
+
+ --1.YOL
+ select TitleOfCourtesy, FirstName,LastName from Employees
+  where TitleOfCourtesy= 'Mr.' 
+  or TitleOfCourtesy= 'Ms.'
+    or TitleOfCourtesy= 'Dr.'
+
+---2.YOL
+ select TitleOfCourtesy, FirstName,LastName from Employees
+ where TitleOfCourtesy in ('Dr.', 'Mr.')
+
+ --1950,1955 ve 1960 yıllarında doganların lıstelenmesı
+ select FirstName,LastName, year (Birthdate ) from Employees
+where year (Birthdate ) in ('1950','1955','1960')           ---1950 dogumlu cıkmayacak cunku kaayıtta yokmus.
+
+ select FirstName,LastName, year (Birthdate ) as Birthyear from Employees
+where year (Birthdate ) in ('1950','1955','1960')  
+order by Birthyear                                    ---bu da dogum tarıhıne gore sıralar tek fark bu.order by 3 de olur.
+
+--TOP KULLANIMI
+
+select top 3  EmployeeID, FirstName,LastName from Employees;
+
+
+select top 5 EmployeeID, FirstName,LastName,Birthdate from Employees
+order by FirstName
+
+--TOP ıfadesı bır sorguda en son calısan kısımdır.Yani öncelıkle sorgumuz calısır ve olusacak sonuc kumesının ılk 5 kaydı alınırı.
+
+--Çalısanları yaslarına gore azalan sırada sıraladıktan sonra olusacak sonuc kumesının %25 lık kısmını lısteyelım.
+
+select top 25 percent  FirstName, LastName, age=year ( getdate()) - year (Birthdate) from Employees --mantıgı 2020-1983 vs
+
+order by age desc;
+
+--LİKE KULLANIMI
+--Adının Michael lıastelenmesı
+
+--1.YOL
+select FirstName, LastName from Employees where FirstName = 'Michael';
+
+--2.YOL
+select FirstName, LastName from Employees where FirstName like 'Michael';
+
+--Adının ılk harfı A ıle baslayanlar
+select FirstName, LastName from Employees where FirstName like 'A%'
+
+--Soyadının son harfı N olanlar
+select FirstName, LastName from Employees where LastName like '%N'
+
+--Adının ılk harfı A veya L olanlar
+--1.YOL
+select FirstName, LastName from Employees where FirstName like 'A%'
+or FirstName like 'L%'
+
+
+select FirstName, LastName from Employees where FirstName like '[AL]%' 
+order by 1
+
+---öDEVLER
+--1) Adının ıcerısınde R veya T harfı olanlar,
+--2-Adının ilk harfı alfabetık olarak J ıle R aralıgında olanlar
+--3- Adı su sekılde olanlar: tAmer, yAsemin, tAner ( A ıle E arasında tek bır karakter)
+--4- Adının ıcerısınde A ıle E arasında 2 karakter olanlar
+--5 Adının ılk ıkı harfı LA,LN,AA veya AN olanlar
+
+--5)
+select FirstName, LastName from Employees where FirstName like '[LA,LN,AA]%' or FirstName like 'AN'
+order by 1
+--1 )
+select FirstName, LastName from Employees where FirstName like  '%R%' or FirstName like '%T%'
+--2)   
+select FirstName, LastName from Employees where FirstName like '[J-R]%'
+--3)
+select FirstName, LastName from Employees where FirstName like '%A_E%'
+--4)
+select FirstName, LastName from Employees where FirstName like '%A__E%'
